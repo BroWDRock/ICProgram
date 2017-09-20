f77ef6cca09c5cead36b5ecf08b7ce3b0aebce57
Deathavenger <javier.renedo1@gmail.com>
2017-09-20 10:06:06 +0200

    inclusion de escribir fichero
    no one expects the spanish inquisition

--- src/translator.cc
+++ src/translator.cc
@@ -12,6 +12,18 @@ long double tiempo(){
 		(long double) usage.ru_utime.tv_usec/1e6);
 }
 
+void escribirFichero(string fichero){
+fstream fich;
+ string nombre="resultado.txt";
+    fich.open(nombre.c_str(),ios::out);
+
+    if(!fich.is_open()){
+        cout<<"no one expects the spanish inquisition"<<endl;
+    }else{
+     fich<<fichero;
+    }
+    fich.close();
+}
 string denc1(string st){
 	int i;
 	int val;
@@ -42,6 +54,7 @@ string enc1(string st){
 	  }
 	  st[i]=val;
 	}
+	escribirFichero(st);
     cout << st << endl;
 	return st;
 }
@@ -66,6 +79,7 @@ string denc2(string st){
 		st[i]=aux%33+32+99;
 	  }
 	}
+	escribirFichero(st);
     cout << st << endl;
 	return st;
 }
@@ -90,6 +104,7 @@ string enc2(string st){
 		st[i]=aux%33+32+99;
 	  }
 	}
+	escribirFichero(st);
     cout << st << endl;
 	return st;
 }
@@ -107,7 +122,7 @@ string denc3(string st){
 		st[i]=val+32;
 	  }
 	}
-    
+    escribirFichero(st);
     cout << st << endl;
 	return st;
 }
@@ -117,12 +132,12 @@ string enc3(string st){
 	int val;
 	for(i=0;i<st.length();i++){
 	  if(st[i]>32){
-		val=st[i]-32;		
+		val=st[i]-32;
 		val=(2*val+3)%91;
 		st[i]=val+32;
 	  }
 	}
-    
+    escribirFichero(st);
     cout << st << endl;
 	return st;
 }
@@ -130,47 +145,46 @@ string enc3(string st){
 void leerFichero(char eleccion)
 {
     ifstream f;
-    fstream fich;
+
     string leer;
     string nombreFichero = "fichero.txt";
-    string nombre="resultado.txt";
-    fich.open(nombre.c_str(),ios::out);
+
     //comprobamos que no se haya introducido el nombre del fichero
     if(nombreFichero == "")
     {
         cout << "Filename: ";
         getline(cin, nombreFichero);
     }
-    
+
     f.open(nombreFichero.c_str());//abrimos el fichero
-    
-    if(f.is_open() && fich.is_open())
+
+    if(f.is_open())
     {
         do
         {
             if(eleccion == '1')
             {
                 getline(f, leer);
-                
-                fich<<enc1(leer)<<endl;
+
+                enc1(leer);
             }
             else if(eleccion == '2')
             {
                 getline(f, leer);
-                 fich<<enc2(leer)<<endl;
+                 enc2(leer);
             }
             else if(eleccion == '3')
             {
                 getline(f, leer);
-                 fich<<enc3(leer)<<endl;
+                 enc3(leer);
             }
             else
             {
                 cout << "\nCagada Monumental" << endl;
             }
-            
+
         } while(f.eof()==false);
-        
+
         f.close();//Cerramos el fichero
         fich.close();
     }
@@ -191,30 +205,30 @@ void encriptar()
         cout << "Type 2" << endl;
         cout << "Type 3\n" << endl;
         cout << "Select: ";
-    
+
         cin >> op;
-        
+
         cout << endl;
-        
+
         switch(op)
         {
             case '1':
                 leerFichero(op);
-                
+
                 break;
-                
+
             case '2':
                 leerFichero('2');
-                
+
                 break;
-                
+
             case '3':
                 leerFichero('3');
                 break;
-                
+
             default: cout << "\nError. Option not valid.\n" << endl;
         }
-        
+
     } while(op != '1' && op != '2' && op != '3');
 }
 
@@ -228,50 +242,50 @@ void menu()
     long double t_inicial, t_final, t_ejecucion;
     char op;
      string nuevo;
-    
+
     do {
         cout << "\n1. Encrypt file." << endl;
         cout << "2. Decrypt file." << endl;
         cout << "3. Exit.\n" << endl;
-        
+
         cout << "Select option: ";
         cin >> op;
-        
+
         switch(op) {
-                
+
             case '1':
                 t_inicial = tiempo();
                 // Llamada a la función de encriptación.
                 encriptar();
                 t_final = tiempo();
-                
+
                 cout << "Time needed to encrypt the file: " << t_final - t_inicial << " s" << endl;
-                
+
                 break;
-                
+
             case '2':
                 t_inicial = tiempo();
                 // Llamada a la función de desencriptación.
                 desencriptar();
                 t_final = tiempo();
-                
+
                 cout << "Time needed to decrypt the file: " << t_final - t_inicial << " s" << endl;
-                
+
                 break;
-                
+
             case '3':
                 cout << "The program has ended." << endl;
                 break;
-                
-                
+
+
             default: cout << "\nError. Option not valid.\n" << endl;
         }
-        
+
     } while(op != '3');
 }
 
 int main()
 {
     menu();
-    
+
 }
