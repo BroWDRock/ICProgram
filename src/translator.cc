3a7061861c43cb7b7c814efc36fd886e8952a5f7
BroWDRock <misterkooo@hotmail.com>
2017-12-19 19:37:37 +0100

    MPI ready, fixed type of encryptation

--- src/translator.cc
+++ src/translator.cc
@@ -5,15 +5,18 @@
 #include <math.h>
 #include <omp.h>
 #include <vector>
-
+#include "mpi.h"
 using namespace std;
 
 
-//constantes de mpi
-#define MAX_PROCESSES 10
+//variables globales MPI
+int rank;
+int size;
+int numCaracteresLinea=200;
+int maxLineas=1000;
 
-
-
+#define MAX_PROCESSES 0
+//------------------------------
 
 const string ERROR_FICH = "Error. Can't open file.";
 
@@ -50,102 +53,109 @@ long long int ex29(int n){
 }
 
 
-string denc1(string st){
-	int i;
-	int val;
-	string nuevo;
+const char * denc1(string st){
+  int i;
+  int val;
+  string nuevo;
 
-	 #pragma omp simd
-	for(i=0;i<st.length();i++){
-	val=st[i];
-	  if (val>32){
-		val=val-4;
-		if(val<33){
-			val=val+126-33;
-		}
-	  }
-	  nuevo+=val;
-	}
-	return nuevo;
+   #pragma omp simd
+  for(i=0;i<st.length();i++){
+  val=st[i];
+    if (val>32){
+    val=val-4;
+    if(val<33){
+      val=val+126-33;
+    }
+    }
+    nuevo+=val;
+  }
+    const char * r = nuevo.c_str();
+    return r;
 }
 
-string enc1(string st){
-	int i;
-	int val;
-	#pragma omp simd
-	for(i=0;i<st.length();i++){
-	val=st[i];
-	  if (val>32){
-		val=val+4;
-		if(val>126){
-			val=val-126+32;
-		}
-	  }
-	  st[i]=val;
-	}
-	return st;
+const char * enc1(string st){
+  int i;
+  int val;
+  #pragma omp simd
+  for(i=0;i<st.length();i++){
+  val=st[i];
+    if (val>32){
+    val=val+4;
+    if(val>126){
+      val=val-126+32;
+    }
+    }
+    st[i]=val;
+  }
+    const char * r = st.c_str();
+    return r;
 }
 
-string denc2(string st){
-	int i;
-	int val;
-	long long int aux;
-	#pragma omp simd
-	for(i=0;i<st.length();i++){
-	  if(st[i]>32){	
-	    val=st[i]-32;
-	    st[i]=ex29(val)%91+32;	
+const char * denc2(string st){
+  int i;
+  int val;
+  long long int aux;
+  #pragma omp simd
+  for(i=0;i<st.length();i++){
+    if(st[i]>32){  
+      val=st[i]-32;
+      st[i]=ex29(val)%91+32;  
 
-	  }
-	}
-	return st;
+    }
+  }
+    const char * r = st.c_str();
+    return r;
 }
 
-string enc2(string st){
-	int i;
-	int val;
-	long long int aux;
-	#pragma omp simd
-	for(i=0;i<st.length();i++){
-	  if(st[i]>32){
-	    val=st[i]-32;
-	    aux=pow(val,5);
- 	    st[i]=aux%91+32;
-	  }	  
-	}
-	return st;
+const char * enc2(string st){
+  int i;
+  int val;
+  long long int aux;
+  #pragma omp simd
+  for(i=0;i<st.length();i++){
+    if(st[i]>32){
+      val=st[i]-32;
+      aux=pow(val,5);
+       st[i]=aux%91+32;
+    }    
+  }
+    const char * r = st.c_str();
+    return r;
 }
 
-string denc3(string st){
-	int i;
-	int val;
-	#pragma omp simd
-	for(i=0;i<st.length();i++){
-	  if(st[i]>32){
-		val=st[i]-33;
-		val=47*(val-3)%93;
-		if(val<0){
-		  val=93+val;
-		}
-		st[i]=val+33;
-	  }
-	}
-	return st;
+const char * denc3(string st){
+  int i;
+  int val;
+  #pragma omp simd
+  for(i=0;i<st.length();i++){
+    if(st[i]>32){
+    val=st[i]-33;
+    val=47*(val-3)%93;
+    if(val<0){
+      val=93+val;
+    }
+    st[i]=val+33;
+    }
+  }
+  const char * r = st.c_str();
+    return r;
 }
 
-string enc3(string st){
-	int i;
-	int val;
-	#pragma omp simd
-	for(i=0;i<st.length();i++){
-	  if(st[i]>32){
-		val=st[i]-33;		
-		val=(2*val+3)%93;
-		st[i]=val+33;
-	  }
-	}
-	return st;
+const char * enc3(string st){
+  int i;
+  int val;
+  #pragma omp simd
+  for(i=0;i<st.length();i++){
+    if(st[i]>32){
+    val=st[i]-33;    
+    val=(2*val+3)%93;
+    st[i]=val+33;
+    }
+  }
+  const char * r = st.c_str();
+    return r;
 }
+
 
 void escribir(vector<string> texto, string out){
    fstream fich;
@@ -169,14 +179,21 @@ bool leerFichero(string eleccion, string in, string out)
     ifstream f;
 
     bool leido = false;
-   
+    int participants; //NUMERO DE PARTICIPANTES EN LOS QUE DIVIDIR el algoritmo
     string leer="";
-    string linea;
     f.open(in.c_str());
     
-    char caracteres[1000][200];  // 1000 lineas de 200 caracteres
+    
+    char caracteres[maxLineas][numCaracteresLinea];  // 1000 lineas de 200 caracteres
+    char linea[numCaracteresLinea];                  //linea
     int contador=0;             // contador de lineas 
 
+     // Setup MPI
+    MPI_Init( NULL, NULL ); //-------------------------------- inicio mpi // podria ponerlo en el main igual pero seguia dando error asi que acote el error
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+    
     if(f.is_open())
     {
        getline(f, leer);
@@ -194,19 +211,67 @@ bool leerFichero(string eleccion, string in, string out)
             caracteres[contador][j]=leer[j];
         }
        
-        }
+        } //fin de leer el fich
+    
          f.close();
          
          //ahora tengo un vector de caracteres [linea(num)] [caracteres de la linea]
          
-         //ahora va el scatter
-         
-         //despues el if infinito ese 
-         
-         // despues el gather
-         
-         //escribir(concatenado, out);
+         //------------------------------------------------------------------------------------
+            
+        if ( size > MAX_PROCESSES ) 
+            participants = MAX_PROCESSES;
+        else 
+            participants = size;
+        
+        if ( (rank < participants) )    {
+        int send_count = MAX_PROCESSES;
+        int recv_count = MAX_PROCESSES;
+ 
+        /* si no soy root (process 0), entonces encript o lo que sea */
+        //no hace falta xk la matriz ya esta hecha 
 
+        /* pasar la matriz a cada proceso una linea char[200] */
+        MPI_Scatter(&caracteres[0][0], send_count, MPI_CHAR, 
+                         &linea[0] , recv_count, MPI_CHAR, 0, MPI_COMM_WORLD);
+        
+          if(eleccion == "1")
+            {
+               //linea=enc1(leer);
+            }
+            else if(eleccion == "2")
+            {
+                //linea=enc2(leer);
+            }
+            else if(eleccion == "3")
+            {
+                 //linea=enc3(leer);
+            }else if(eleccion == "4")
+            {
+                //linea=denc1(leer);
+            }else if(eleccion == "5")
+            {
+                //linea=denc2(leer);
+            }else if(eleccion == "6")
+            {
+                //linea=denc3(leer);
+            }
+            else
+            {
+                cout << "\nCagada Monumental" << endl;
+            } 
+           
+            MPI_Gather(&linea, recv_count, MPI_CHAR, &caracteres[0][0], send_count, MPI_CHAR, 0, MPI_COMM_WORLD);
+           
+           /* 
+            MPI_Scatter(globaldata, 1, MPI_INT, &localdata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Gather(&localdata, 1, MPI_INT, globaldata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            */
+        
+        }
+ MPI_Finalize(); ////////////////////////////---- fin de mpi
+        //-------------------------------------------------------------------------------------------------------
+       
          leido = true;
     }
     else
@@ -218,79 +283,13 @@ bool leerFichero(string eleccion, string in, string out)
 }
 
 
-//--------------------------------------------------------------------------------fin mpi metodo
-/*
-void procesarLinea(string linea, string input, string output){
-    int i=0;
-    char opc;
 
-    while(i<linea.length()){
 
-        while(i<linea.length() && linea[i]!=' '){
-            input+=linea[i];
-            i++;
-        }
-        
-        i++;
-
-        while(i<linea.length() && linea[i]!=' '){
-            output+=linea[i];
-            i++;
-        }
-        
-        i++;
-
-        opc=linea[i];
-
-        i++;
-    }
-
-    leerFichero(opc, input, output);
-}
-*/
-
-/*
-void procesarFichero(string input, string output, char op){
-    ifstream fich;
-    string linea, in="", out="";
-    int chunksize, tid;
-    vector<string> lineas;
-
-    chunksize = numberlines(input);
-    fich.open(input.c_str());
-      if (fich.is_open()){
-
-        for(int i = 0; i < chunksize; i++)
-        {
-            getline(fich, linea);
-            lineas.push_back(linea);
-            
-        }
-    }
-    
-    if(fich.is_open())
-    {
-        #pragma omp parallel for num_threads(chunksize) private(in, out, linea)
-            for(int x=0; x<lineas.size()-1; x++){
-                linea = lineas[x];
-
-                #pragma omp parallel
-                {
-                leerFichero(op, input, output);
-                }
-            }
-    }   
-}
-*/
 
 int main(int argc, char *argv[]) {
     long double t_inicial, t_final;
     
-    // Setup MPI
-    MPI_Init( &argc, &argv ); //-------------------------------- inicio mpi
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-
+   
     if(argc != 4) {
         cout << "Syntax Error." << endl;
         cout << "Correct syntax: <executable> <input_file> <output_file> option" << endl;
@@ -306,28 +305,9 @@ int main(int argc, char *argv[]) {
 
         cout << "Time: " << t_final - t_inicial << " s" << endl;
     }
-          MPI_Finalize(); ////////////////////////////---- fin de mpi
+          
 }
 
-/*
-int main(int argc, char *argv[]) {
-    long double t_inicial, t_final;
-
-    if(argc != 2) {
-        cout << "Syntax Error." << endl;
-        cout << "Correct syntax: <executable> <index_file>" << endl;
-    }
-    else {
-        string index = argv[1];
-
-        t_inicial = tiempo();º
-        procesarFichero(index);
-        t_final = tiempo();
-
-        cout << "Time: " << t_final - t_inicial << " s" << endl;
-    }
-}
-*/
 
 
 
